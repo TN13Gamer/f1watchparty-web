a325c572bf47bfad860857fe64d0013982d98f44
TN13Gamer <89504837+TN13Gamer@users.noreply.github.com>
2026-01-21 19:31:59 +0530

    Setup project and ignore sensitive config

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.example.js
+// Rename this file to config.js and add your actual keys
+const firebaseConfig = {
+         apiKey: "AIzaSyDddLbKsv7BCiXQeFy9jzuFrmuWv8qt3DM",
+        authDomain: "f1-stream-live.firebaseapp.com",
+        projectId: "f1-stream-live",
+        storageBucket: "f1-stream-live.firebasestorage.app",
+        messagingSenderId: "426806288804",
+        appId: "1:426806288804:web:f8ee225776596ebc39e131",
+        measurementId: "G-50FMFWY32X"
+};
