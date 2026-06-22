8aee377a9e3bf0f758597155fe67c03712a09502
TN13Gamer <89504837+TN13Gamer@users.noreply.github.com>
2026-06-23 01:45:22 +0530

    fix: remove hello.js test function to stay within Vercel Hobby 12-function limit

--- api/hello.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.method === 'OPTIONS') return res.status(200).end();
-  
-  res.status(200).send('Hello World from Vercel Backend!');
-};
